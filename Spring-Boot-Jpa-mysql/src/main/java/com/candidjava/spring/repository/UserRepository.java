b162e292ec4d6a577c52d014f70f42d8a96beb33
dell <dell@DESKTOP-T43RPRT>
2019-11-05 18:45:30 +0530

    i added one record

--- Spring-Boot-Jpa-mysql/src/main/java/com/candidjava/spring/repository/UserRepository.java
+++ Spring-Boot-Jpa-mysql/src/main/java/com/candidjava/spring/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.candidjava.spring.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.candidjava.spring.bean.User;
-public interface UserRepository extends CrudRepository<User, Long>
-{
+public interface UserRepository extends CrudRepository<User, Long>{
 	
+	List<User> findByName(String name);
 
 }
